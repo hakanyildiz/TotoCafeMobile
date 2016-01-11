17940a06643573b0304efc5f177da1b9c683e15d
Hakan YILDIZ <hakanhknyldz@gmail.com>
2016-01-11 19:07:35 +0200

    navigation-drawer added.

--- app/src/main/java/com/sohos/totocafemobile/MainActivity.java
+++ app/src/main/java/com/sohos/totocafemobile/MainActivity.java
@@ -3,6 +3,7 @@ package com.sohos.totocafemobile;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -16,9 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
          toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+
+        drawerFragment.setUp(R.id.fragment_navigation_drawer ,(DrawerLayout)findViewById(R.id.drawer_layout) ,toolbar);
 
     }
 
