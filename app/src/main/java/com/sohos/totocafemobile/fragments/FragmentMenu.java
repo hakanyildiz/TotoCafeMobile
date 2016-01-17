bdd474867504f465036fe7ff3c2c64893d1d66a4
Hakan YILDIZ <hakanhknyldz@gmail.com>
2016-01-17 12:17:32 +0200

    menu creation part1

--- app/src/main/java/com/sohos/totocafemobile/fragments/FragmentMenu.java
+++ app/src/main/java/com/sohos/totocafemobile/fragments/FragmentMenu.java
@@ -1,32 +1,57 @@
 package com.sohos.totocafemobile.fragments;
 
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.sohos.totocafemobile.MyApplication;
 import com.sohos.totocafemobile.R;
+import com.sohos.totocafemobile.network.VolleySingleton;
+import com.sohos.totocafemobile.pojo.CategoryTable;
+import com.sohos.totocafemobile.restful.JSONParser;
+import com.sohos.totocafemobile.restful.RestAPI;
 
-/**
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.xml.transform.ErrorListener;
+
+
+public class FragmentMenu extends /**
  * A simple {@link Fragment} subclass.
  * Use the {@link FragmentMenu#newInstance} factory method to
  * create an instance of this fragment.
- */
-public class FragmentMenu extends Fragment {
+ */Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-
+    private VolleySingleton volleySingleton;
+    private ImageLoader imageLoader;
+    private RequestQueue requestQueue;
+    ArrayList<String> data;
+    ArrayList<CategoryTable> myCategoryTable = new ArrayList<CategoryTable>();
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -49,6 +74,7 @@ public class FragmentMenu extends Fragment {
         // Required empty public constructor
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +82,99 @@ public class FragmentMenu extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        //volleySingleton = VolleySingleton.getInstance();
+        //requestQueue = volleySingleton.getRequestQueue();
+        //sendJsonRequest();
+        
+        boolean menuStatus = MyApplication.readFromPreferences(getActivity(), "menuStatus", false);
+        int companyID = MyApplication.readFromPreferences(getActivity(), "companyID", -1);
+
+        if (menuStatus) { // true ise demekki qr okuma falan başarılı.. Categorileri gösterme vakti
+            getCompaniesFromService(companyID);
+        }
+
+    }
+
+    private void getCompaniesFromService(int companyID) {
+        new AsyncLoadCategoriesOfCompany().execute(companyID);
+
+    }
+    protected class AsyncLoadCategoriesOfCompany extends AsyncTask<Integer, JSONObject, ArrayList<CategoryTable>> {
+        ArrayList<CategoryTable> cateTable = null;
+
+        @Override
+        protected ArrayList<CategoryTable> doInBackground(Integer... params) {
+            // TODO Auto-generated method stub
+
+            RestAPI api = new RestAPI();
+            try {
+
+                JSONObject jsonObj = api.getCategoriesOfCompany(params[0]);
+
+                JSONParser parser = new JSONParser();
+
+                cateTable = parser.parseCategory(jsonObj);
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                Log.d("AsyncLoadDeptDetails", e.getMessage());
+
+            }
+
+            return cateTable;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<CategoryTable> result) {
+            // TODO Auto-generated method stub
+            CategoryTable myCategory = new CategoryTable();
+            for (int i = 0; i < result.size(); i++) {
+                data.add(result.get(i).getCategoryID() + " " + result.get(i).getCategoryName());
+
+                myCategory.setCategoryID(result.get(i).getCategoryID());
+                myCategory.setCategoryName(result.get(i).getCategoryName());
+                myCategory.setAvailabilityID(result.get(i).getAvailabilityID());
+                myCategory.setCompanyID(result.get(i).getCompanyID());
+
+                myCategoryTable.add(myCategory);
+
+            }
+
+            //adapter.notifyDataSetChanged();
+
+            Toast.makeText(getContext(),"Loading Completed",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    private void sendJsonRequest() {
+        String url = "";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, (String)null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response){
+                parseJSONResponse(response);
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                volleyError.printStackTrace();
+
+                Log.d("Error = ", volleyError.toString());
+
+
+            }
+        });
+        requestQueue.add(request);
+    }
+
+    private void parseJSONResponse(JSONObject response) {
+        if(response == null || response.length() == 0){
+            return;
+        }
+
     }
 
     @Override
@@ -68,12 +187,7 @@ public class FragmentMenu extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
-      boolean menuStatus =  MyApplication.readFromPreferences(getActivity(), "menuStatus", false);
-      int companyID =  MyApplication.readFromPreferences(getActivity(), "companyID", -1);
-
-      if(menuStatus){ // true ise demekki qr okuma falan başarılı.. Categorileri gösterme vakti
-
-      }
+       
 
     }
 }
