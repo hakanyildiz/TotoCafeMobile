97410b476ee6b2a795ce6cf807efcfe0565b7f45
okan öztaban <okanoztaban@anadolu.edu.tr>
2016-01-18 12:12:48 +0200

    last check
    dsafaff

--- app/src/main/java/com/sohos/totocafemobile/restful/RestAPI.java
+++ app/src/main/java/com/sohos/totocafemobile/restful/RestAPI.java
@@ -1,7 +1,5 @@
-/* JSON API for totoCafe Android Appliation */
-package com.sohos.totocafemobile.restful;
+package com.sohos.totocafemobile.restful;/* JSON API for totoCafe Android Appliation */
 
-import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,19 +24,19 @@ public class RestAPI {
     private final String urlString = "http://totowebservices.azurewebsites.net/totoHandler.ashx";
 
     private static String convertStreamToUTF8String(InputStream stream) throws IOException {
-	    String result = "";
-	    StringBuilder sb = new StringBuilder();
-	    try {
+        String result = "";
+        StringBuilder sb = new StringBuilder();
+        try {
             InputStreamReader reader = new InputStreamReader(stream, "UTF-8");
             char[] buffer = new char[4096];
             int readedChars = 0;
             while (readedChars != -1) {
                 readedChars = reader.read(buffer);
                 if (readedChars > 0)
-                   sb.append(buffer, 0, readedChars);
+                    sb.append(buffer, 0, readedChars);
             }
             result = sb.toString();
-		} catch (UnsupportedEncodingException e) {
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
         return result;
@@ -47,9 +45,6 @@ public class RestAPI {
 
     private String load(String contents) throws IOException {
         URL url = new URL(urlString);
-		
-		Log.d("myURL", url.toString());
-
         HttpURLConnection conn = (HttpURLConnection)url.openConnection();
         conn.setRequestMethod("POST");
         conn.setConnectTimeout(60000);
@@ -65,45 +60,45 @@ public class RestAPI {
 
 
     private Object mapObject(Object o) {
-		Object finalValue = null;
-		if (o.getClass() == String.class) {
-			finalValue = o;
-		}
-		else if (Number.class.isInstance(o)) {
-			finalValue = String.valueOf(o);
-		} else if (Date.class.isInstance(o)) {
-			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss", new Locale("en", "USA"));
-			finalValue = sdf.format((Date)o);
-		}
-		else if (Collection.class.isInstance(o)) {
-			Collection<?> col = (Collection<?>) o;
-			JSONArray jarray = new JSONArray();
-			for (Object item : col) {
-				jarray.put(mapObject(item));
-			}
-			finalValue = jarray;
-		} else {
-			Map<String, Object> map = new HashMap<String, Object>();
-			Method[] methods = o.getClass().getMethods();
-			for (Method method : methods) {
-				if (method.getDeclaringClass() == o.getClass()
-						&& method.getModifiers() == Modifier.PUBLIC
-						&& method.getName().startsWith("get")) {
-					String key = method.getName().substring(3);
-					try {
-						Object obj = method.invoke(o, null);
-						Object value = mapObject(obj);
-						map.put(key, value);
-						finalValue = new JSONObject(map);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
+        Object finalValue = null;
+        if (o.getClass() == String.class) {
+            finalValue = o;
+        }
+        else if (Number.class.isInstance(o)) {
+            finalValue = String.valueOf(o);
+        } else if (Date.class.isInstance(o)) {
+            SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss", new Locale("en", "USA"));
+            finalValue = sdf.format((Date)o);
+        }
+        else if (Collection.class.isInstance(o)) {
+            Collection<?> col = (Collection<?>) o;
+            JSONArray jarray = new JSONArray();
+            for (Object item : col) {
+                jarray.put(mapObject(item));
+            }
+            finalValue = jarray;
+        } else {
+            Map<String, Object> map = new HashMap<String, Object>();
+            Method[] methods = o.getClass().getMethods();
+            for (Method method : methods) {
+                if (method.getDeclaringClass() == o.getClass()
+                        && method.getModifiers() == Modifier.PUBLIC
+                        && method.getName().startsWith("get")) {
+                    String key = method.getName().substring(3);
+                    try {
+                        Object obj = method.invoke(o, null);
+                        Object value = mapObject(obj);
+                        map.put(key, value);
+                        finalValue = new JSONObject(map);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
-		}
-		return finalValue;
-	}
+        }
+        return finalValue;
+    }
 
     public JSONObject CreateNewUser(String Name,String Surname,String Email,String Password,String BirthDate,double GenderID) throws Exception {
         JSONObject result = null;
@@ -227,8 +222,23 @@ public class RestAPI {
         result = new JSONObject(r);
         return result;
     }
-	
-	public JSONObject InsertToOrder(double amount,double ControllerID,double ProductID,String OrderDetails) throws Exception {
+
+    public JSONObject InsertUserToCostumerTable(double UserID,double UserTypeID) throws Exception {
+        JSONObject result = null;
+        JSONObject o = new JSONObject();
+        JSONObject p = new JSONObject();
+        o.put("interface","RestAPI");
+        o.put("method", "InsertUserToCostumerTable");
+        p.put("UserID",mapObject(UserID));
+        p.put("UserTypeID",mapObject(UserTypeID));
+        o.put("parameters", p);
+        String s = o.toString();
+        String r = load(s);
+        result = new JSONObject(r);
+        return result;
+    }
+
+    public JSONObject InsertToOrder(double amount,double ControllerID,double ProductID,String OrderDetails) throws Exception {
         JSONObject result = null;
         JSONObject o = new JSONObject();
         JSONObject p = new JSONObject();
@@ -238,21 +248,6 @@ public class RestAPI {
         p.put("ControllerID",mapObject(ControllerID));
         p.put("ProductID",mapObject(ProductID));
         p.put("OrderDetails",mapObject(OrderDetails));
-        o.put("parameters", p);
-        String s = o.toString();
-        String r = load(s);
-        result = new JSONObject(r);
-        return result;
-    }
-	
-    public JSONObject InsertUserToCostumerTable(double UserID,double UserTypeID) throws Exception {
-        JSONObject result = null;
-        JSONObject o = new JSONObject();
-        JSONObject p = new JSONObject();
-        o.put("interface","RestAPI");
-        o.put("method", "InsertUserToCostumerTable");
-        p.put("UserID",mapObject(UserID));
-        p.put("UserTypeID",mapObject(UserTypeID));
         o.put("parameters", p);
         String s = o.toString();
         String r = load(s);
